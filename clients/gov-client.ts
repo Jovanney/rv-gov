2961592400e7dbaf0d675ae77a2cb106eb8b8159
Jovanney <100790418+Jovanney@users.noreply.github.com>
2025-03-11 21:02:21 -0300

    feat: gov ky client

--- clients/gov-client.ts
+++ clients/gov-client.ts
@@ -2,4 +2,7 @@ import ky from "ky";
 
 export const govClient = ky.create({
   prefixUrl: process.env.NEXT_PUBLIC_GOV_API_URL,
+  headers: {
+    "chave-api-dados": process.env.NEXT_PUBLIC_GOV_API_KEY,
+  },
 });
